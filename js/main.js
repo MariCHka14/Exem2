d13aefac5041720ebba44638d424dc2500c4a44e
MariCHka14 <sms-mar.s.140282@gmail.com>
2017-05-21 18:02:11 +0300

    Exem2

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  $('.bo-team-slider').slick({
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+  	autoplaySpeed: 4000,
+  	
+  });
+});
+
+$('.bo-header-nav').on('click', 'a', function(enent){
+	enent.preventDefault();
+	var link = $(this).attr('href');
+
+	var distanse = $(link).offset().top;
+	$('html, body').animate({
+		scrollTop: distanse
+	}, 500);
+})
+
+$(window).scroll(function(){
+	var dist = $('#about').offset().top;
+		if ($(window).scrollTop() >= dist){ 
+			$('.ch-container-logo').css('background', 'rgba(255, 255, 255, 0.7)');
+				
+		}else{
+			$('.ch-container-logo').css('background', 'transparent');
+		}
+});
+
+
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-item-size',
+  percentPosition: true,
+  gutter: 5,
+});
+
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    // use outer width of grid-sizer for columnWidth
+    columnWidth: '.grid-item-size',
+    gutter: 5,
+  }
+})
+
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+
+
+
